e5890955e029889c60f68257ba0e0af5288748fb
Mahoo12138 <mahoo12138@outlook.com>
2022-10-03 13:25:59 +0800

    feat: 添加杯子组件及喝水宝主页面

---
+++ miniprogram/packages/water/components/Cup.ts
@@ -0,0 +1,26 @@
+// packages/water/components/Cup.ts
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        image: String,
+        time: String,
+        water: Number,
+        unit: String
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
